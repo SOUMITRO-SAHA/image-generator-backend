679c1824aabd5a1854acd45da3d3c96fb4c39a5a
Soumitra Saha <soumitrosahaofficial@gmail.com>
2023-10-06 17:07:04 +0530

    Maid the Upload Folder Public

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import connect from './config/database.js';
-import userRoutes from './routes/user.routes.js';
 import imageRoutes from './routes/image.routes.js';
+import userRoutes from './routes/user.routes.js';
 
 // Establishing the Database connection
 connect();
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(express.json());
@@ -20,6 +25,9 @@ app.use(
   })
 );
 app.use(morgan('tiny'));
+
+// Making the Upload Folder Public:
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
 app.use(userRoutes);
